72abd1896028064bc11c4daccfeec2aef2f0cce1
HidenYT <buharev2009@gmail.com>
2024-02-10 16:31:37 +0300

    Updated poetry dependencies for running deeplabcut

--- src/core/settings.py
+++ src/core/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
-from turtle import back
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
