873b371b5f8e56748ade0347fa8b5e7a40b6e8df
HidenYT <buharev2009@gmail.com>
2024-02-15 12:53:04 +0300

    Created user field in InferredKeypoints and TrainedNeuralNetwork models

--- src/model_inference/models.py
+++ src/model_inference/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from django.db import models
 from model_training.models import TrainedNeuralNetwork
 from video_manager.models import InferenceVideo
+from django.contrib.auth.models import User
 
 
 class InferredKeypoints(models.Model):
@@ -13,3 +14,5 @@ class InferredKeypoints(models.Model):
     trained_neural_network = models.ForeignKey(TrainedNeuralNetwork, on_delete=models.DO_NOTHING, related_name='inferred_keypoints_set')
 
     inference_video = models.ForeignKey(InferenceVideo, on_delete=models.DO_NOTHING, related_name='inferred_keypoints_set')
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='inferred_keypoints_set')
--- src/model_training/models.py
+++ src/model_training/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.db import models
 from train_datasets_manager.models import TrainDataset
+from django.contrib.auth.models import User
 
 
 class NeuralNetworkType(models.Model):
@@ -30,3 +31,5 @@ class TrainedNeuralNetwork(models.Model):
     train_dataset = models.ForeignKey(TrainDataset, on_delete=models.DO_NOTHING, related_name='trained_neural_networks')
 
     neural_network_type = models.ForeignKey(NeuralNetworkType, on_delete=models.DO_NOTHING, related_name='trained_neural_networks')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='trained_neural_networks')
